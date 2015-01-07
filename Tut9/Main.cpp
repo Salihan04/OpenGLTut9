72d4fd0f64736b7430695056520911ed56ad3729
Salihan04 <salihan04@gmail.com>
2015-01-07 14:06:52 +0800

    Drew 4 red points

--- Tut9/Main.cpp
+++ Tut9/Main.cpp
@@ -1,9 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-using namespace std;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/freeglut.h>
+
+#include "ShaderLoader.h"
+
+void createPointsBuffer();
+void renderScene();
+
+GLuint programID;
+GLuint pointsBuffer;
 
 int main(int argc, char **argv) {
+	//initialise GLFW
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialise GLFW\n");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, 0);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow *window = glfwCreateWindow(700, 500, "OpenGLTut9 - Geometry Shading", NULL, NULL);
+	if (window == NULL){
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetWindowPos(window, 400, 200);
+
+	//initialize GLEW
+	glewExperimental = true;		// Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	//need to be after glewInit(), otherwise got error
+	programID = LoadShaders("VertexShader.vertexshader", "FragmentShader.fragmentshader");
+
+	createPointsBuffer();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, GL_TRUE);
+
+		renderScene();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 	return 0;
+}
+
+void createPointsBuffer() {
+	GLfloat points[] = {
+		-0.45f, 0.45f,
+		0.45f, 0.45f,
+		0.45f, -0.45f,
+		-0.45f, -0.45f,
+	};
+
+	glGenBuffers(1, &pointsBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, pointsBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+}
+
+void renderScene() {
+	// Render frame
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//use the shader
+	glUseProgram(programID);
+
+	// Specify layout of point data
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, pointsBuffer);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_POINTS, 0, 4);
+
+	glDisableVertexAttribArray(0);
 }
